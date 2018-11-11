e0db67b705bfe73f9e969bd13e8dad73644c4bdc
edevrim <edevrim92@gmail.com>
2018-11-12 00:25:15 +0100

    Update waveletShrinkage_hard.m

--- waveletShrinkage_hard.m
+++ waveletShrinkage_hard.m
@@ -3,9 +3,6 @@ function shrinked = waveletShrinkage_hard(x, N ,iter)
 %N is the type of DB wavelet
 %iter is the number of iterations
 
-% c = [1/sqrt(2), 1/sqrt(2)]; %Haar used changed to DB
-% d = [1/sqrt(2), -1/sqrt(2)]; 
-
 %DB transform 
 x_deb = db_lp_hp(x, N, iter); 
 
@@ -14,19 +11,6 @@ x_deb = db_lp_hp(x, N, iter);
 %b will be the first b term (last and longest in the x_deb - C 
 
 b = x_deb(len/2 + 1: len); %always be the half of the length 
-
-% [row1, len] = size(x);
-% [a, b] = dwlt(x,c,d); 
-% init = a; 
-% C = [b];
-% 
-% for i=1:(iter - 1)
-%     [a1, b1] = dwlt(init,c,d);
-%     init = a1; 
-%     C = [b1 C]; 
-% end 
-% 
-% C = [a1 C];
 
 sigma = mad(b,1)/0.6745; %median absolute deviation, use the finest b is the beginning one
 lambda = sigma*sqrt(2*log(len)); 
@@ -42,9 +26,3 @@ end
 shrinked = inv_db_lp_hp(x_deb, N, iter);
 
 end
-    
-
-
-
-
-
