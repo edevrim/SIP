07a51d6669209f10c1a961ee530235fb381d178e
edevrim <edevrim92@gmail.com>
2018-11-12 00:23:56 +0100

    Update plotfft.m

--- plotfft.m
+++ plotfft.m
@@ -1,12 +1,10 @@
 function [] = plotfft(fft, fs)
 
-f1 = (0:length(fft)-1) * fs / length(fft); %makes the plot in 0 to 10 (fs) to see where the amplitude is 
-
-%While the amplitude tells you how strong is a harmonic in a signal, the phase tells where this harmonic lies in time.
+f1 = (0:length(fft)-1) * fs / length(fft); 
 
 figure,
 subplot(2, 1, 1);
-plot(f1, abs(fft)) %abs function gives the magnitude (|a+ib|)
+plot(f1, abs(fft)) %abs gives the magnitude (|a+ib|)
 title('amplitutde spectrum')
 subplot(2, 1, 2);
 plot(f1, unwrap(angle(fft))) % angle gives the phase (loc) 
